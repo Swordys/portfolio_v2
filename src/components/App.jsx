813b9593fa87290593bbbff79cc7756e34b6d0ec
Daviti <davit_saginashvili@yahoo.com>
2017-02-07 20:41:25 +0400

    inital commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="main-wrap">
+        <h2>Yoo</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
