5494947ef93bae0683658bc70e34e1ab6dd21ab1
Daviti <davit_saginashvili@yahoo.com>
2017-02-16 23:38:47 +0400

    about section transtion styling

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -5,16 +5,27 @@ import { connect } from 'react-redux';
 class AboutContent extends React.Component {
   render() {
     const { aboutState } = this.props;
+    let toggleCompo = '';
     let toggleClass = '';
+    let togglePic = '';
+    let toggleFrame = '';
     if (aboutState) {
+      toggleCompo = 'about-compo-active';
       toggleClass = 'contentActive';
+      togglePic = 'about-picture-active';
+      toggleFrame = 'about-frame-active';
     }
     return (
       <div className={`about-content ${toggleClass}`}>
-        <div className="about-compo">
-          <div className="about-picture"></div>
+        <div className={`about-compo ${toggleCompo}`}>
+          <div className="pic-wrap">
+            <div className={`about-picture ${togglePic}`}>
+              <div className={`about-frame ${toggleFrame}`}>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="about-compo">
+        <div className={`about-compo ${toggleCompo}`}>
           <div className="about-text">
 
           </div>
