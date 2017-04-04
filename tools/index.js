4cb1bbb88b0a340aa4f33e35af1ec64a9a24d9ee
Daviti <davit_saginashvili@yahoo.com>
2017-04-04 22:21:19 +0400

    changed port

--- tools/index.js
+++ tools/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 const app = express();
 
 /* eslint-disable no-console*/
